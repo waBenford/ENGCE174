57c465bd6ac194dbaba1eb3761c7ae1d4922f4ca
waBenford <thanakorngithub@gmail.com>
2025-11-21 11:03:30 +0700

    Complete

---
+++ javaEclipse/javaLab1/src/javaLab1/Lab1_5.java
@@ -0,0 +1,25 @@
+package javaLab1;
+import java.util.Scanner;
+
+public class Lab1_5 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner( System.in );
+
+		System.out.print( "" );
+		int Number = scanner.nextInt();
+		
+		if ( Number == 1 ) {
+			System.out.println( "Americano" );
+		} else if ( Number == 2 ) {
+			System.out.println( "Latte" );
+		} else if ( Number == 3 ) {
+			System.out.println( "Espresso" );
+		} else if ( Number == 4 ) {
+			System.out.println( "Mocha" );
+		} else {
+			System.out.println( "Only number 1-4 Please try agian" );
+		}
+	}
+
+}
