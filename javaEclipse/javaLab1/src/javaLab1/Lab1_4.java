980976931f86385fb7de14ad9452944af8e7e574
waBenford <thanakorngithub@gmail.com>
2025-11-21 16:38:03 +0700

    Complete

--- javaEclipse/javaLab1/src/javaLab1/Lab1_4.java
+++ javaEclipse/javaLab1/src/javaLab1/Lab1_4.java
@@ -9,16 +9,19 @@ public class Lab1_4 {
 		System.out.print( "" );
 		int Score = scanner.nextInt();
 		
-		if ( Score >= 80 && Score <= 100 ) {
+		if ( Score < 0 || Score > 100 ) {
+			System.out.println( "Invalid Score" );
+		}
+		else if ( Score >= 80 ) {
 			System.out.println( "A" );
 		}
-		else if ( Score >= 70 && Score <= 79 ) {
+		else if ( Score >= 70 ) {
 			System.out.println( "B" );
 		}
-		else if ( Score >= 60 && Score <= 69 ) {
+		else if ( Score >= 60 ) {
 			System.out.println( "C" );
 		}
-		else if ( Score >= 50 && Score <= 59 ) {
+		else if ( Score >= 50 ) {
 			System.out.println( "D" );
 		}
 		else {
