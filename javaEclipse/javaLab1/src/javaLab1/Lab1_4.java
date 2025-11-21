076170d8ca3c22474d9a117f0598d187181bb1f9
waBenford <thanakorngithub@gmail.com>
2025-11-21 16:29:54 +0700

    Complete

--- javaEclipse/javaLab1/src/javaLab1/Lab1_4.java
+++ javaEclipse/javaLab1/src/javaLab1/Lab1_4.java
@@ -6,18 +6,22 @@ public class Lab1_4 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner( System.in );
 		
-		System.out.print( "input your score: " );
+		System.out.print( "" );
 		int Score = scanner.nextInt();
 		
-		if ( Score > 79 ) {
+		if ( Score >= 80 && Score <= 100 ) {
 			System.out.println( "A" );
-		}else if ( Score > 69 ) {
+		}
+		else if ( Score >= 70 && Score <= 79 ) {
 			System.out.println( "B" );
-		}else if ( Score > 59 ) {
+		}
+		else if ( Score >= 60 && Score <= 69 ) {
 			System.out.println( "C" );
-		}else if ( Score > 49 ) {
+		}
+		else if ( Score >= 50 && Score <= 59 ) {
 			System.out.println( "D" );
-		}else {
+		}
+		else {
 			System.out.println( "F" );
 		}
 	}
