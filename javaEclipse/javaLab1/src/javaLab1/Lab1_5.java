6aa723f86e72a894481df8bb5285946e505bb7a2
waBenford <thanakorngithub@gmail.com>
2025-11-23 16:54:06 +0700

    Complete

--- javaEclipse/javaLab1/src/javaLab1/Lab1_5.java
+++ javaEclipse/javaLab1/src/javaLab1/Lab1_5.java
@@ -4,18 +4,18 @@ import java.util.Scanner;
 public class Lab1_5 {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner( System.in );
+		Scanner getValue = new Scanner( System.in );
 
 		System.out.print( "Choose menu: " );
-		int Number = scanner.nextInt();
+		int num = getValue.nextInt();
 		
-		if ( Number == 1 ) {
+		if ( num == 1 ) {
 			System.out.println( "Americano" );
-		} else if ( Number == 2 ) {
+		} else if ( num == 2 ) {
 			System.out.println( "Latte" );
-		} else if ( Number == 3 ) {
+		} else if ( num == 3 ) {
 			System.out.println( "Espresso" );
-		} else if ( Number == 4 ) {
+		} else if ( num == 4 ) {
 			System.out.println( "Mocha" );
 		} else {
 			System.out.println( "Invalid Menu" );
